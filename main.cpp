bc07a015d0e1b293f7dbb68dacd57dc838b2798f
Jean-Yves Roda <ji.igrec@gmail.com>
2017-01-16 09:06:03 +0100

    Ajout du mode non canonique

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <termios.h>
 
 
 using namespace std;
@@ -125,6 +126,27 @@ void getWindowSize(CTerminalSize & Size) {
     /* size.ws_row is the number of rows, size.ws_col is the number of columns. */
     Size.first = size.ws_row;
     Size.second = size.ws_col;
+}
+
+
+//http://www.gnu.org/software/libc/manual/html_node/Noncanon-Example.html
+void set_input_mode (void)
+{
+  struct termios tattr;
+
+  /* Make sure stdin is a terminal. */
+  if (!isatty (STDIN_FILENO))
+  {
+      fprintf (stderr, "Not a terminal.\n");
+      exit (EXIT_FAILURE);
+  }
+
+  /* Set the funny terminal modes. */
+  tcgetattr (STDIN_FILENO, &tattr);
+  tattr.c_lflag &= ~(ICANON|ECHO); /* Clear ICANON and ECHO. */
+  tattr.c_cc[VMIN] = 0;
+  tattr.c_cc[VTIME] = 3;
+  tcsetattr (STDIN_FILENO, TCSAFLUSH, &tattr);
 }
 
 
