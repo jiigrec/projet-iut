1894ae7b6d05cfa7b13dd89bff38a70032d48b25
jiigrec <ji.igrec@gmail.com>
2017-01-18 21:19:57 +0100

    Ajout des configurations, menus, aide, fichiers de langue, et debut de commentaires

--- main.cpp
+++ main.cpp
@@ -8,50 +8,48 @@
 #include <dirent.h> 
 #include <stdio.h> 
 #include <random>
-#include "sound.h"
 #include <fstream>
 #include <iomanip>
 #include <string>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
 
-//Temporaire, a changer avec les fichiers de config
-const vector <string> VParamChar {"KTokenPlayer1", "KTokenPlayer2"};
-const vector <string> VParamString {"KColorPlayer1", "KColorPlayer2"};
-const vector <string> VParamUnsigned {"KSizeLine", "KSizeSpace"};
-const char KUP ('z');
-const char KDOWN ('s');
-const char KLEFT ('q');
-const char KRIGHT ('d');
-const char KUP2 ('o');
-const char KDOWN2 ('l');
-const char KLEFT2 ('k');
-const char KRIGHT2 ('m');
+//Index des options de configuration
+const vector <string> VControls {"J1Up", "J1Down", "J1Left", "J2Right", "J2Up", "J2Down", "J2Left", "J2Right", "pause"};
+const vector <string> VColors {"ColorPlayer1", "ColorPlayer2","ColorFood"};
+const string languageSetting = "langue";
+const string foodSetting = "food";
 
-const char KPAUSE ('g');
-
-const char KFOOD ('#');
-//Fin des valeurs temporaires
-
+//Constantes de l'affichage du personnage et des blancs
 const char KPlayerUp ('^');
 const char KPlayerDown ('v');
 const char KPlayerLeft ('<');
 const char KPlayerRight ('>');
+const char KEmpty (' ');
 
 //classe myparams
 struct CMyParam {
-    map <string, char> MapParamChar;
-    map <string, unsigned> MapParamUnsigned;
-    map <string, string> MapParamString;
+    map <string, char> Controls;
+    char food;
+    map <string, string> Colors;
+    map <string, string> GameStrings;
+    bool IA;
+    unsigned difficulty;
+    unsigned randomFrequency;
 };
 
+
+//Efface l'écran
 void ClearScreen ()
 {
     cout << "\033[H\033[2J";
 }
 
+
+//Liste des couleurs disponibles
 const string KReset   ("0");
 const string KNoir    ("30");
 const string KRouge   ("31");
@@ -61,54 +59,32 @@ const string KBleu    ("34");
 const string KMAgenta ("35");
 const string KCyan    ("36");
 
-void LoadParams(CMyParam & MyParams) {
-    ifstream ifs("config.yalm");
-    string str;
-    char c;
-    unsigned nb;
-    for(string cle; ifs >> cle;) {
-        if (find(VParamChar.begin(), VParamChar.end(), cle) != VParamChar.end()) {
-            ifs >> c;
-            MyParams.MapParamChar[cle] = c;
-            getline(ifs, str);
-        }
-        else if (find(VParamUnsigned.begin(), VParamUnsigned.end(), cle) ,VParamUnsigned.end()) {
-            ifs >> nb;
-            MyParams.MapParamUnsigned[cle] = nb ;
-        }
-        else if(find(VParamString.begin(), VParamString.end(), cle) ,VParamString.end()) {
-            ifs >> str;
-            MyParams.MapParamString[cle] = str ;
-        }
-    }
-}
 
-template <typename T, typename U>
-void ShowMap (const map <T, U>& MyMap) {
-    for ( const auto & Val : MyMap) {
-        cout << Val.first << " " << Val.second << endl;
-        cout << endl;
-    }
-}
-
+//Change la couleur des prochains caractères affichés
 void Couleur (const string & coul)
 {
     cout << "\033[" << coul <<"m";
 }
 
+
+//Types de la grille de jeu
 typedef vector <char> CVLine; // un type représentant une ligne de la grille
 typedef vector <CVLine> CMatrix; // un type représentant la grille
 typedef pair   <unsigned, unsigned> CPosition; // un type représentant une coordonnée dans la grille
 
 typedef pair <unsigned, unsigned> CTerminalSize; // Un type qui représente la taille de la fenetre
 
-//Caractères par défaut
-const char KTokenPlayer1 = 'X';
-const char KTokenPlayer2 = 'O';
-const char KEmpty        = ' ';
-
 //Stockage des paramètres du Terminal, pour changer le mode entre canonique et non canonique
 struct termios saved_attributes;
+
+/*Variables de configuration, initialisées avec leurs valeurs par défaut
+bool IA = false;
+unsigned difficulty = 1;
+int randomFrequency = 3; */
+
+
+//Variable contenant les paramètres
+CMyParam Settings;
 
 
 //Variables utilisées pendant le jeu
@@ -117,28 +93,123 @@ CPosition Player1;
 CPosition Player2;
 unsigned ScoreJ1;
 unsigned ScoreJ2;
-int randomFrequency = 1;
-int randomCounter = 0;
 
 //Variables qui gardent le temps
 unsigned long Time;
 unsigned EndTime;
+unsigned randomCounter = 0;
 
 
 
-pair <unsigned, string> displayMenu( vector<string> & Items) {
-    cout << "Menu : " << endl;
-    unsigned i;
-    for (i = 1; i <= Items.size(); ++i)
-        cout << i << " : " << Items[i - 1] << endl;
-    cout << "Entrez votre choix :" << endl;
-    int choix;
-    cin >> choix;
-    while (true)
-        if (choix <= 0 || choix > Items.size())
-            return  pair<unsigned, string> (i - 1, Items[i - 1]);
+//Donne la liste des fichiers de langue présents
+vector<string> getLanguages() {
+   vector<string> Langues;
+   /* Source : http://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c */
+   DIR  *d;
+  struct dirent *dir;
+  d = opendir("./Nos_fichiers/langue");
+  if (d)
+  {
+    string File;
+    while ((dir = readdir(d)) != NULL)
+    {
+      File = dir->d_name;
+      if (File.find(".yaml") < File.size())
+          Langues.push_back(File);
+    }
+
+    closedir(d);
+  }
+    return Langues;
 }
 
+
+//Définit les chaines localisées en fonction d'un fichier de langue
+void setLanguage(string & LanguageFile) {
+    ifstream stream ("Nos_fichiers/langue/" + LanguageFile);
+    string str;
+    string key;
+    while (!stream.eof()) {
+        stream >> key;
+        stream >> str;
+        stream >> str;
+        replace(str.begin(), str.end(), '_', ' ');
+        Settings.GameStrings[key] = str;
+    }
+    stream.close();
+}
+
+//Définit la langue à partir d'une locale simplifiée, comme FR pour francais
+void setLanguageFromLocale(string & Locale) {
+      vector<string> Languages = getLanguages();
+      for (unsigned i = 0; i < Languages.size(); ++i) {
+          if (Languages[i].find(Locale) < Languages[i].size()) {
+              setLanguage(Languages[i]);
+              return;
+          }
+      }
+}
+
+//Charge les paramètres du fichier config.yaml
+void LoadParams(CMyParam & MyParams) {
+    ifstream stream("Nos_fichiers/config.yaml");
+    string str;
+    char c;
+    string key;
+    while (!stream.eof()) {
+        stream >> str;
+        if (str == "#") {
+            stream >> str;
+            while (str != "#")
+                stream >> str;
+            stream >> str;
+        }
+        if (str == foodSetting) {
+            stream >> c;
+            stream >> c;
+            MyParams.food = c;
+        }
+        if (str == languageSetting) {
+            stream >> str;
+            stream >> str;
+            setLanguageFromLocale(str);
+        }
+        if (find(VColors.begin(), VColors.end(), str) != VColors.end()) {
+            key = str;
+            stream >> str;
+            stream >> str;
+            MyParams.Colors[key] = str;
+        }
+        if (find(VControls.begin(), VControls.end(), str) != VControls.end()) {
+            key = str;
+            stream >> c;
+            stream >> c;
+            MyParams.Controls[key] = c;
+        }
+    }
+    stream.close();
+}
+
+
+
+//Donne le code couleur à partir d'une couleur en language commun
+string getColor(string & color) {
+    if (color ==  "noir")
+        return KNoir;
+    if (color == "rouge")
+        return KRouge;
+    if (color == "vert")
+        return KVert;
+    if (color == "jaune")
+        return KJaune;
+    if (color == "bleu")
+        return KBleu;
+    if (color == "magenta")
+        return KMAgenta;
+    if (color == "cyan")
+        return KCyan;
+    return KReset;
+}
 
  void MenuTuto()
     {
@@ -161,26 +232,101 @@ pair <unsigned, string> displayMenu( vector<string> & Items) {
         }
  }
 
-vector<string> getDirectoryContents() {
-    vector<string> Langues;
-    /* Source : http://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c */
-   DIR  *d;
-  struct dirent *dir;
-  d = opendir("./langue");
-  if (d)
-  {
-    string File;
-    while ((dir = readdir(d)) != NULL)
-    {
-      File = dir->d_name;
-      if (File.find(".yaml") < File.size())
-          Langues.push_back(File);
-    }
 
-    closedir(d);
-  }
-    return Langues;
+ //Règle la difficulté du jeu
+ void setDifficulty() {
+     int choix = 0;
+     while (choix < 1 || choix > 3) {
+         cout << "Choisir la difficulté: " << endl;
+         cout << "1. " << "Facile" << endl;
+         cout << "2. " << "Moyen" << endl;
+         cout << "3. " << "Difficile" << endl;
+         cout << "Entrez votre choix " << ":" << endl;
+         cin >> choix;
+     }
+     Settings.difficulty = choix;
+     Settings.randomFrequency = 4 - choix;
+ }
+
+ void languageMenu() {
+
+ }
+
+//Affiche le fichier d'aide
+ void showHelp() {
+     ClearScreen();
+     ifstream stream ("Nos_fichiers/langue/" + Settings.GameStrings["HelpFile"]);
+     string str;
+     while (!stream.eof()) {
+         getline(stream, str);
+         cout << str << endl;
+     }
+     char c = 'y';
+     cout << "Appuyez sur 1 puis entrée pour revenir." << endl;
+     while (c != '1') {
+         cin >> c;
+     }
+
+ }
+
+//Enregistre les options choisies
+ bool setOption(int & Choix) {
+     switch (Choix) {
+     case 1:
+         Settings.IA = !Settings.IA;
+         return false;
+         break;
+     case 2:
+         setDifficulty();
+         return false;
+         break;
+     case 3:
+         languageMenu();
+         return false;
+         break;
+     case 4:
+         showHelp();
+         return false;
+         break;
+     case 5:
+         return true;
+     default:
+         return true;
+         break;
+     }
+ }
+
+
+ //Affiche le menu principal
+bool mainMenu() {
+    bool ready = false;
+    while (!ready) {
+        cout << "Menu : " << endl << "1. ";
+        if (Settings.IA)
+            cout << "Mode 1 joueur" << ".";
+        else
+            cout << "Mode 2 joueurs" << ".";
+        cout << " Choisissez pour changer" << "." << endl;
+        cout << "2. " << "Changer difficulté"<<". "<<"Actuellement "<<": " << Settings.difficulty << endl;
+        cout << "3. " << "Changer la langue" << endl;
+        cout << "4. " << "Aide" << endl;
+        cout << "5. " << "Jouer" << endl;
+        cout << "6. " << "Quitter" << endl;
+        cout << "Entrez votre choix :" << endl;
+        int choix;
+        cin >> choix;
+        while (true)
+            if (choix == 5)
+                return false;
+            if (choix <= 0 || choix > 5)
+                continue;
+        ready = setOption(choix);
+    }
+    return true;
+
 }
+
+
 
 
 /* Code copié depuis :
@@ -217,20 +363,27 @@ void set_input_mode (void)
 
 /* Fin du code copié */
 
+
+//Démarre le chronomètre
 void startTimer() {
     Time = clock();
 }
 
+//Donne le nombre de secondes écoulées depuis le début du chrono
 unsigned getSecondsElapsed() {
     return ((unsigned) clock() - Time) / CLOCKS_PER_SEC;
 }
 
+//Donne le temps restant des joueurs
 int getTimeLeft() {
     return EndTime - getSecondsElapsed();
 }
 
+
+//Mets le jeu en pause
 void gamePause() {
     ClearScreen();
+    unsigned elapsedTime = getSecondsElapsed();
     reset_input_mode();
     char c = 'y';
     cout << "Jeu en pause. Appuyez sur 1 puis entrée pour reprendre." << endl;
@@ -238,11 +391,13 @@ void gamePause() {
         cin >> c;
     }
     set_input_mode();
+    Time = clock() - elapsedTime * CLOCKS_PER_SEC;
     ClearScreen();
 }
 
+//Fait apparaitre un trésor sur la carte
 void popFood(CMatrix & Mat) {
-    if (randomCounter == randomFrequency) {
+    if (randomCounter == Settings.randomFrequency) {
         randomCounter = 0;
         random_device rd;
         mt19937 mt(rd());
@@ -252,16 +407,16 @@ void popFood(CMatrix & Mat) {
         while (food == Player1 || food == Player2) {
             food = pair<int, int> (vert(mt), hor(mt));
         }
-        Mat[food.first][food.second] = KFOOD;
+        Mat[food.first][food.second] = Settings.food;
     } else
         ++randomCounter;
 }
 
-
+//Affiche l'interface du jeu ainsi que la grille
 void  ShowMatrix (const CMatrix & Mat) {
     ClearScreen();
     Couleur(KReset);
-    cout << "Temps restant : " << getTimeLeft() << " Score J1: " << ScoreJ1 << " Score J2: " << ScoreJ2;
+    cout << "Temps restant " << ": " << getTimeLeft() << " Score J1" << ": " << ScoreJ1 << " Score J2" << ": " << ScoreJ2 << endl;
     for (unsigned i = 0; i <= Mat[0].size() + 1; ++i) cout << "=";
     cout << endl;
     for (unsigned i = 0; i < Mat.size(); ++i) {
@@ -280,9 +435,9 @@ void  ShowMatrix (const CMatrix & Mat) {
                 cout << Mat[i][j];
                 Couleur(KReset);
             }
-            if (Mat[i][j] == KFOOD) {
+            if (Mat[i][j] == Settings.food) {
                 Couleur(KBleu);
-                cout << KFOOD;
+                cout << Settings.food;
                 Couleur(KReset);
             }
         }
@@ -294,7 +449,7 @@ void  ShowMatrix (const CMatrix & Mat) {
 }
 
 
-
+//Initialise la grille de jeu
 void InitMat (CMatrix & Mat, unsigned NbLine, unsigned NbColumn, CPosition & PosPlayer1, CPosition & PosPlayer2) {
     Mat.resize(NbLine);
     CVLine Line;
@@ -306,17 +461,19 @@ void InitMat (CMatrix & Mat, unsigned NbLine, unsigned NbColumn, CPosition & Pos
         Mat[i] = Line;
     }
     PosPlayer1 = {0,0};
-    Mat[PosPlayer1.first][PosPlayer1.second] = KTokenPlayer1;
+    Mat[PosPlayer1.first][PosPlayer1.second] = KPlayerDown;
     PosPlayer2 = {NbLine - 1, NbColumn - 1};
-    Mat[PosPlayer2.first][PosPlayer2.second] = KTokenPlayer2;
+    Mat[PosPlayer2.first][PosPlayer2.second] = KPlayerUp;
 }
 
 
-
+//Retourne si le joueur est le joueur 1 ou non, en fonction de sa position
 bool isFirstPlayer(CPosition & Pos) {
     return (Pos == Player1); //Expression booléenne
 }
 
+
+//Fonction qui gère les collisions
 bool checkEat(char & Object, CPosition & PosObject,char & Player, CPosition & PosPlayer) {
     switch (Object) {
     case KPlayerDown:
@@ -338,71 +495,57 @@ bool checkEat(char & Object, CPosition & PosObject,char & Player, CPosition & Po
         else
             ScoreJ2 = ScoreJ2 + 5;
         break;
-      case KFOOD:
-        if (isFirstPlayer(PosPlayer))
-            ++ScoreJ1;
-        else
-            ++ScoreJ2;
-        break;
+
     default:  // Pas un joueur
+        if (Object == Settings.food) {
+            if (isFirstPlayer(PosPlayer))
+                ++ScoreJ1;
+            else
+                ++ScoreJ2;
+        }
         break;
     }
 
     return true;
 }
 
+
+//Fonction pour déplacer un joueur
 void MoveToken (CMatrix & Mat, char Move, CPosition  & Pos) {
     CPosition OldPos = Pos;
     char Player;
     Mat[Pos.first][Pos.second] = KEmpty;
-    switch (Move) {
-    case KUP:
-    case KUP2 : if (Pos.first > 0) Pos.first = Pos.first - 1;
+    if (Move == Settings.Controls["J1Up"] || Move == Settings.Controls["J2Up"]) {
+        if (Pos.first > 0) Pos.first = Pos.first - 1;
         Player = KPlayerUp;
-        break;
-    case KLEFT:
-    case KLEFT2 : if (Pos.second) Pos.second = Pos.second - 1;
-        Player = KPlayerLeft;
-        break;
-    case KRIGHT:
-    case KRIGHT2 : if (Pos.second < Mat[0].size() - 1) Pos.second = Pos.second + 1;
-        Player = KPlayerRight;
-        break;
-    case KDOWN:
-    case KDOWN2 : if (Pos.first < Mat.size() - 1) Pos.first = Pos.first + 1;
-        Player = KPlayerDown;
-        break;
-    default: //Ne rien faire
-        break;
     }
+    if (Move == Settings.Controls["J1Left"] || Move == Settings.Controls["J2Left"]) {
+        if (Pos.second) Pos.second = Pos.second - 1;
+        Player = KPlayerLeft;
+    }
+    if (Move == Settings.Controls["J1Right"] || Move == Settings.Controls["J2Right"]) {
+        if (Pos.second < Mat[0].size() - 1) Pos.second = Pos.second + 1;
+        Player = KPlayerRight;
+    }
+    if (Move == Settings.Controls["J1Down"] || Move == Settings.Controls["J2Down"]) {
+        if (Pos.first < Mat.size() - 1) Pos.first = Pos.first + 1;
+        Player = KPlayerDown;
+    }
+
     if (Mat[Pos.first][Pos.second] != KEmpty)
         if (!checkEat(Mat[Pos.first][Pos.second], Pos, Mat[OldPos.first][OldPos.second], OldPos))
             Pos = OldPos;
     Mat[Pos.first][Pos.second] = Player;
 }
 
-
+//Fonction pour lire les touches pressées
 void readInput(char & Input) {
-    switch (Input) {
-    case KUP:
-    case KDOWN:
-    case KLEFT:
-    case KRIGHT:
+    if (Input == Settings.Controls["J1Up"] || Input == Settings.Controls["J1Down"] || Input == Settings.Controls["J1Left"] || Input == Settings.Controls["J1Right"])
         MoveToken(Mat, Input, Player1);
-        break;
-    case KUP2:
-    case KDOWN2:
-    case KLEFT2:
-    case KRIGHT2:
+    if (Input == Settings.Controls["J2Up"] || Input == Settings.Controls["J2Down"] || Input == Settings.Controls["J2Left"] || Input == Settings.Controls["J2Right"])
         MoveToken(Mat, Input, Player2);
-        break;
-    case KPAUSE:
+    if (Input == Settings.Controls["pause"])
         gamePause();
-        break;
-    default:
-        // Ne rien faire
-        break;
-    }
 }
 
 
@@ -414,12 +557,24 @@ void getWindowSize(CTerminalSize & Size) {
     Size.second = size.ws_col;
 }
 
-
-void win(unsigned Player, unsigned & Score1, unsigned & Score2) {
+// Affiche le menu des résultats
+bool win(unsigned Player, unsigned & Score1, unsigned & Score2) {
         cout << "Félicitations au Joueur " << Player << " ! Il gagne avec " << Score1 << " points contre " << Score2 << "points." << endl;
+        while (true) {
+            cout << "1. " << "Revenir au menu" << endl;
+            cout << "2. " << "Quitter" << endl;
+            int choix;
+            cin >> choix;
+            if (choix == 1)
+                return true;
+            if (choix == 2)
+                return false;
+        }
 }
 
-int ppal () {
+
+// Boucle d'une partie jouée
+bool ppal () {
       bool Playing = true;
       CTerminalSize WindowSize;
       getWindowSize(WindowSize);
@@ -438,22 +593,24 @@ int ppal () {
       }
       reset_input_mode();
       if (ScoreJ1 > ScoreJ2)
-          win(1, ScoreJ1, ScoreJ2);
+         return win(1, ScoreJ1, ScoreJ2);
       else
-          win(2, ScoreJ2, ScoreJ1);
-
-      return 0;
+         return win(2, ScoreJ2, ScoreJ1);
 }
 
 
 
 
 
-int main(int argc, char *argv[])
+int main()
 {
-    //Menu ici, le jeu utilisera les reglages stockés dans des variables globales
+    LoadParams(Settings);
+    bool playing = true;
+    while(playing) {
+        playing = mainMenu();
+        playing = ppal();
+    }
 
-    ppal();
 
     return 0;
 }
